9950a180d506ac27497daa50d324c0350e8936d9
sfilbertf <filbertfebrianto14@gmail.com>
2023-06-09 16:48:34 +0200

    Visualize incremental improvements to the NNF

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "pm.h"
-#include "pm_helper.h"
 
 int main(int, char** argv) {
     std::string A = std::string(argv[1]);
@@ -11,18 +10,21 @@ int main(int, char** argv) {
     pm::ImageMatrix A_matrix = pm::ConvMat2Matrix(A_mat);
     pm::ImageMatrix B_matrix = pm::ConvMat2Matrix(B_mat);
 
-    pm::PatchMatch::GetInstance().Initialize(A_matrix, B_matrix, 1);
+    for(int t = -1; t < 4; ++t) {
+        if(t == -1) {
+            pm::PatchMatch::GetInstance().Initialize(A_matrix, B_matrix, 1);
+        } else {
+            pm::PatchMatch::GetInstance().Iterate(t % 2 == 0, 0.5, 1);
+        }
 
-    for(int t = 0; t < 4; ++t) {
-        pm::PatchMatch::GetInstance().Iterate(t % 2 == 0, 0.5, 1);
+        pm::ImageMatrix A_reconstructed_matrix = pm::PatchMatch::GetInstance().Reconstruct();
+
+        cv::Mat A_reconstructed_mat = pm::ConvMatrix2Mat(A_reconstructed_matrix);
+        cv::cvtColor(A_reconstructed_mat, A_reconstructed_mat, cv::COLOR_BGR2RGB);
+
+        cv::imshow("A_reconstructed", A_reconstructed_mat);
+        cv::waitKey(0);
     }
-
-    pm::ImageMatrix A_reconstructed_matrix = pm::PatchMatch::GetInstance().Reconstruct();
-    cv::Mat A_reconstructed_mat = pm::ConvMatrix2Mat(A_reconstructed_matrix);
-    cv::cvtColor(A_reconstructed_mat, A_reconstructed_mat, cv::COLOR_BGR2RGB);
-
-    cv::imshow("A_reconstructed", A_reconstructed_mat);
-    cv::waitKey(0);
 
     return 0;
 }
